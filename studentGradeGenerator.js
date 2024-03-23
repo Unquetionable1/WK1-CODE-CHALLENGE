6ca7da874f8fe21c963b1c796c81424d08504457
Edmound <eddnjoori@gmail.com>
2024-03-23 11:02:40 +0300

    I have a student grade generator

---
+++ studentGradeGenerator.js
@@ -0,0 +1,22 @@
+const grade=75;
+function generateGrade(grade){
+    if (grade>=0 && grade<=100) {
+        return gradeGenerator(grade);
+    } else {
+        return 'The vallue input is invalid.Please input a value between 0 and 100'
+    }
+}
+
+function gradeGenerator(grade){
+    if (grade>79) {
+        return 'A';
+    } else if(grade>=60 && grade<= 79){
+        return 'B';
+    } else if (grade>=49 && grade <= 59) {
+        return 'c';
+    } else if(grade>=40 && grade<49){
+        return 'D';
+    } else {
+        return 'E';
+    }
+}
